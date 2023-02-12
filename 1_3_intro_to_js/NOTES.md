3af461a6faf8da2cf5c53bb118cfe893250dbb41
Kai Prenger <kai.prenger@gmail.com>
2023-02-12 13:30:08 -0500

    added bar chart that is upside down...still troubleshooting

--- 1_3_intro_to_js/NOTES.md
+++ 1_3_intro_to_js/NOTES.md
@@ -12,3 +12,12 @@ Sometimes it helps to formulate what you understood and where you got stuck in o
 If you find you're not able complete this week's assignment, reflecting on where you are getting stuck here will help you get full credit for this week's tutorial
 
 ------------
+
+This bonus exercise from 1_2 and 1_3 made me realize that I struggle with two
+concepts:
+
+- viewBox
+- positioning in general (see upside down barchart)
+
+I think the top to right positioning is getting me confused.
+
--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -6,3 +6,43 @@ const incrementClicks = () => {
     numClicks += 1;
     document.getElementById('click-count').innerHTML = numClicks;
 }
+
+const margin = {top: 30, right: 30, bottom: 70, left: 60},
+width = 460 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
+
+const svg = d3.select("#bar-chart")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+d3.csv("us-presidential-popular-vote-1904.csv").then( function(data) {
+        // set x-axis
+        const x = d3.scaleBand()
+            .range([ 0, width ])
+            .domain(data.map(d => d.Party))
+            .padding(0.2);
+        svg.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
+        // set y-axis
+        const y = d3.scaleLinear()
+            .domain([0, 9000000])
+            .range([height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+        // draw bars
+        svg.selectAll("bars")
+            .data(data)
+            .join("rect")
+                .attr("x", d => x(d.Party))
+            .attr("y", d => y(d.Value))
+            .attr("width", x.bandwidth())
+            .attr("height", d => height - y(d.Votes))
+            .attr("fill", "#ff9999")
+        
+})
