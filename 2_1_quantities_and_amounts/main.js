afbdf8bef3d020743017436d1f02ee59f074f264
miasza <mia.szarvas@gmail.com>
2023-02-14 17:09:00 -0500

    add class roster and set up notes for class

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,12 +1,9 @@
-
-/* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+console.log(d3)
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
-  .then(data => {
-    console.log("data", data)
+d3.csv('roster.csv', d3.autoType)
+  .then(roster => {
+    console.log("roster", roster)
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
@@ -15,4 +12,29 @@ d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
 
+    // select container
+
+    // append table
+
+    // append table head
+
+    // append table body
+
+    // use d3 to populate table
+
+    // select table rows, fill with roster data
+
+    // assign 'student' class
+
+    // assign id
+
+    // assign text
+
+    // break the chain!
+
+    // add cell for first name to row
+
+    // add cell for last name to row
+
+    // inspect all arguments - data, index, nodes
   })
