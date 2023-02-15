f67f672cf60fa422116f379c211e472dc7f6da4b
Kai Prenger <kai.prenger@gmail.com>
2023-02-14 19:57:50 -0500

    WIP

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -4,9 +4,37 @@
 // const height = ;
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
+d3.csv('../data/squirrelActivities.csv', d3.autoType)
   .then(data => {
     console.log("data", data)
+
+    // select container
+    const table = d3.select('#container')
+      .append("table")
+
+      // append table head by breaking chain
+      table.append("thead")
+
+      const tbody = table.append("tbody")
+
+      // use d3 
+      const row = tbody.selectAll("tr")
+        .data(data)
+        .join("tr")
+        // assign 'squirrelActivities' class
+        .attr("class", "squirrelActivities")
+        // .attr("id", data => data.count)
+
+        // break the chain
+        row
+        .append("td")
+        .text(data => data.activity)
+
+        row
+        .append("td")
+        .text(data => data.count)
+
+
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
