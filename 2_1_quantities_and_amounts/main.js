91fe30ecd883094d848b087a543d48ac02920196
miasza <mia.szarvas@gmail.com>
2023-02-14 20:28:54 -0500

    add class notes from tutorial 2_1

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -2,8 +2,8 @@ console.log(d3)
 
 /* LOAD DATA */
 d3.csv('roster.csv', d3.autoType)
-  .then(roster => {
-    console.log("roster", roster)
+  .then(classRoster => {
+    console.log("roster", classRoster)
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
@@ -13,28 +13,45 @@ d3.csv('roster.csv', d3.autoType)
     /** Select your container and append the visual elements to it */
 
     // select container
-
     // append table
+    const table = d3.select("#container")
+      .append("table")
+      
+     // append table head
+    table.append("thead")
+        // append table body
 
-    // append table head
+    const tbody = table.append("tbody")
+      // .append("tr")
+      // .attr('class', 'row')
 
-    // append table body
 
     // use d3 to populate table
+    const row = tbody.selectAll("tr")
+      .data(classRoster)
+      .join("tr")
+      // assign 'student' class
+      .attr("class", "student") 
+      // assign id
+      .attr("id", data => data.Last)
 
-    // select table rows, fill with roster data
-
-    // assign 'student' class
-
-    // assign id
 
     // assign text
 
     // break the chain!
-
     // add cell for first name to row
+    row.append("td")
+      .text(data => data.First)
 
     // add cell for last name to row
+    row
+      .append("td")
+      .text(data => data.Last)
+      
+    // add cell for last name to row
+    row
+      .append("td")
+      .text(data => data.Num)
 
-    // inspect all arguments - data, index, nodes
   })
+
