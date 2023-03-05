fe41196eca9136d5a00aff7fb6e92181087ece98
Kai Prenger <kai.prenger@gmail.com>
2023-03-05 15:46:44 -0500

    created basic scales

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,16 +1,29 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+const width = window.innerWidth * 0.6;
+const height = window.innerHeight * 0.6;
+margin = 50;
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
+d3.csv('../data/squirrelActivities.csv', d3.autoType)
   .then(data => {
     console.log("data", data)
 
-    /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
-    
+    // test out how to access domain from array of objects
+    activity = [...data.map((d => d.activity))]
+    console.log('activity', activity)
+    // nice and easy
 
+    /* SCALES */
+    // x scale
+    const xScale = d3.scaleBand()
+      .domain([...data.map((d => d.activity))])
+      .range([margin, width - margin])
+    
+    // y scale
+    const yScale = d3.scaleLinear()
+      .domain([0, Math.max(...data.map(d => d.count))])
+      .range([height - margin, margin])
+    
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
 
