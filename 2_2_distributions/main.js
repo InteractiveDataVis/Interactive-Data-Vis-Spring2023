67742a4f71292ef0730a5f60a8795fd7de548a06
Kai Prenger <kai.prenger@gmail.com>
2023-03-12 13:31:52 -0400

    setting globals and constaints

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,8 +1,8 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = {top: 20, bottom:40, left: 40, right:40},
+  radius = 4;
 
 /* LOAD DATA */
 d3.csv("[PATH_TO_YOUR_DATA]", d3.autoType)
