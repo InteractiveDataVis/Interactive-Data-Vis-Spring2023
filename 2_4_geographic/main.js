74c403888e05816e7ef3f402a4c473c53d128a20
miasza <mia.szarvas@gmail.com>
2023-03-14 19:53:32 -0400

    add class notes

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-// const width = window.innerWidth * 0.9,
-//   height = window.innerHeight * 0.7,
-//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+const width = window.innerWidth * 0.9,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 /**
  * LOAD DATA
@@ -11,16 +11,50 @@
   d3.json("../data/usState.json"),
   d3.csv("../data/stateCapitals.csv", d3.autoType),
 ]).then(([geojson, capitals]) => {
-  
+
+  // INSPECT DATA
+  console.log('geojson', geojson)
+  console.log('capitals', capitals)
+
+  // APPEND SVG
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+
   // SPECIFY PROJECTION
+  const projection = d3.geoAlbersUsa()
+    .fitSize([width, height], geojson)
  
 
   // DEFINE PATH FUNCTION
+  const pathGen = d3.geoPath(projection)
 
 
   // APPEND GEOJSON PATH  
-  
+  const states = svg.selectAll("path.states")
+    .data(geojson.features)
+    .join("path")
+    .attr("class", "states")
+    .attr("d", coords => pathGen(coords))
+    .attr("fill", "transparent")
+    .attr("stroke", "black")
+
+
   
   // APPEND DATA AS SHAPE
+  const capitalCircles = svg.selectAll("circle.capital")
+    .data(capitals)
+    .join("circle")
+    .attr("class", "capital")
+    .attr("r", 5)
+    .attr("fill", "pink")
+    .attr("transform", (d) => {
+      const [x, y] = projection([d.longitude, d.latitude])
+      return `translate(${x}, ${y})`
+    })
 
 });
+
+
