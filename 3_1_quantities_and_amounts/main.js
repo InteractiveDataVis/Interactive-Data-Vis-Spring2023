101a4edd7d63f0f12c7807975a5fed44581f0e64
miasza <mia.szarvas@gmail.com>
2023-03-21 19:18:37 -0400

    add class notes for 3_1

--- 3_1_quantities_and_amounts/main.js
+++ 3_1_quantities_and_amounts/main.js
@@ -1,20 +1,20 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight,
+  margin = { top: 20, bottom: 50, left: 60, right: 40 };
+  radius = 10;
 
 // // since we use our scales in multiple functions, they need global scope
-// let xScale, yScale;
+let xScale, yScale;
 
 /* APPLICATION STATE */
 let state = {
-  // data: [],
+  data: [],
 };
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
-  console.log("data", raw_data);
+d3.csv("../data/squirrelActivities.csv", d3.autoType).then(raw_data => {
+  console.log("raw_data", raw_data);
   // save our data to application state
   state.data = raw_data;
   init();
@@ -23,8 +23,17 @@ d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in
 function init() {
-  /* SCALES */
 
+  console.log(state.data.map(d => d.activity))
+  /* SCALES */
+  xScale = d3.scaleBand()
+    .domain(state.data.map(d => d.activity))
+    .range([margin.left, width - margin.right])
+    .paddingInner(0.2)
+
+  yScale = d3.scaleLinear()
+    .domain([0, d3.max(state.data, d => d.count)])
+    .range([height - margin.bottom, margin.top])
 
 
   draw(); // calls the draw function
@@ -34,7 +43,20 @@ function init() {
 // we call this every time there is an update to the data/state
 function draw() {
   /* HTML ELEMENTS */
+    // define svg
+    const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+    const rect = svg
+      .selectAll("rect.bar")
+      .data(state.data)
+      .join("rect")
+      .attr("class", "bar") // remember the fourth brother!
+      .attr("width", xScale.bandwidth())
+      .attr("x", d => xScale(d.activity))
+      .attr("y", d => yScale(d.count))
+      .attr("height", d => height - yScale(d.count))
  
-
-
 }
