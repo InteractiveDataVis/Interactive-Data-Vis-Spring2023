3f4c5c81b8b8eaee04f7a0c88b0c08ccba3a75ec
Kai Prenger <kai.prenger@gmail.com>
2023-04-08 16:54:52 -0400

    drawing axes

--- project/main.js
+++ project/main.js
@@ -5,45 +5,72 @@ const width = window.innerWidth * 0.7,
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
-// let svg;
-// let xScale;
-// let yScale; ...
+let svg
+let xScale
+let yScale
+let xAxis
+let yAxis
+let xAxisGroup
+let yAxisGroup
 
 /* APPLICATION STATE */
 let state = {
     data: [],
     selection: "All", // + YOUR FILTER SELECTION
-  };
+  }
   
   /* LOAD DATA */
   // + SET YOUR DATA PATH
-  d3.csv('../data/migration_flows_from_2010_to_2019.csv', d3.autoType)
+  d3.csv('../data/migration_flows_from_2010_to_2019.csv', d => {
+    return {
+        year: new Date(+d.year, 0, 1),
+        number_of_people: d.number_of_people
+    }
+  })
     .then(raw_data => {
       console.log('loaded data:', raw_data)
       state.data = raw_data
       console.log('state_data', state.data)
-    //   init();
-    });
+      init()
+    })
   
   /* INITIALIZING FUNCTION */
   // this will be run *one time* when the data finishes loading in
   function init() {
     // + SCALES
-  
+    xScale = d3.scaleTime()
+        .domain(d3.extent(state.data, d => d.year))
+        .range([margin.right, width - margin.left])
+    
+    yScale = d3.scaleLinear()
+        .domain(d3.extent(state.data, d => d.number_of_people))
+        .range([height - margin.bottom, margin.top])
   
     // + AXES
+    const xAxis = d3.axisBottom(xScale)
+    const yAxis = d3.axisLeft(yScale)
   
   
     // + UI ELEMENT SETUP
   
   
     // + CREATE SVG ELEMENT
-    
+    svg = d3.select('#container')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
   
     // + CALL AXES
+    xAxisGroup = svg.append('g')
+        .attr('transform', `translate(0, ${height - margin.bottom})`)
+        .call(xAxis)
+    
+    yAxisGroup = svg.append('g')
+        .attr('transform', `translate(${margin.left}, 0)`)
+        .call(yAxis)
   
   
-    draw(); // calls the draw function
+    draw() // calls the draw function
   }
   
   /* DRAW FUNCTION */
@@ -51,7 +78,7 @@ let state = {
   function draw() {
     // + FILTER DATA BASED ON STATE
     const filteredData = state.data
-      // .filter(d => d.country === state.selection)
+      .filter(d => d.current_state === 'Utah')
   
     // + UPDATE SCALE(S), if needed
     
