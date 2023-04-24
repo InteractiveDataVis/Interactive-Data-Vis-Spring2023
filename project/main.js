a8ce407d7d63d6d8b32cc7018515cb84c856d0b8
Kai Prenger <kai.prenger@gmail.com>
2023-04-24 14:56:21 -0400

    drawing axes

--- project/main.js
+++ project/main.js
@@ -36,6 +36,7 @@ but need access to in draw(); all these variables are empty before we assign
 something to them.
 */
 
+let colorScale
 let svg
 let xScale
 let yScale
@@ -78,57 +79,94 @@ d3.csv('../data/migration_flows_from_2010_to_2019.csv', d => {
   - 
   */
   function init() {
+        // filter Utah data
+        const utahData = state.data
+        .filter(d => d.current_state === 'Utah')
+      console.log('Utah data >>', utahData) // diagnostic
+  
+      // group data by year; create objects with years as keys
+      // 
+      const groupedByYear = d3.rollup(utahData, i => {
+        const firstInstance = i[0]
+        return {
+          from_different_state_total: firstInstance.from_different_state_total,
+          abroad_total: firstInstance.abroad_total,
+          utah_total: firstInstance.population,
+        }
+      }, d => d.year)
+  
+      console.log('grouped by year >>', groupedByYear) // diagnostic
+  
+      // Convert Map objects to array of objects
+      const aggByYear = Array.from(groupedByYear, ([year, values]) =>
+        ({year: new Date(year), ...values}))
+  
+      console.log('agg by year >>', aggByYear) // diagnostic
+  
+      let totalGrowthFromDifferentStates = 0
+      let totalGrowthFromAbroad = 0
+      let totalGrowthInState = 0 
+      let previousYearPop = 0
+  
+      aggByYear.forEach(d => {
+        totalGrowthFromDifferentStates += d.from_different_state_total
+        totalGrowthFromAbroad += d.abroad_total
+  
+        if (previousYearPop > 0) {
+          totalGrowthInState += d.utah_total - previousYearPop
+        }
+        previousYearPop = d.utah_total
+      })
+  
+      // prep data for barchart
+      const simpleChartData = [
+        { typeOfMigration : 'From Different States', value : totalGrowthFromDifferentStates },
+        { typeOfMigration : 'From Abroad', value : totalGrowthFromAbroad },
+        { typeOfMigration : 'Internal Growth', value : totalGrowthInState },
+      ]
+      
+      console.log('bar chart data >>', simpleChartData)
+
+    // create scales
+    xScale = d3.scaleBand()
+      .domain(simpleChartData.map(d => d.typeOfMigration))
+      .range([0, width - margin.right])
+      .paddingInner(0.2)
+    
+    yScale = d3.scaleLinear()
+      // nice() function: https://www.d3indepth.com/scales/
+      .domain([0, d3.max(simpleChartData, d => d.value)]).nice()
+      .range([height - margin.bottom, margin.top])
+
+
+    
+    // set the color scale
+    colorScale = d3.scaleOrdinal()
+      .range([maroon, teal, lightGrey])
+
+    svg = d3.select('#container')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+
+    // assign axes to scales
+    xAxis = d3.axisBottom(xScale)
+    yAxis = d3.axisLeft(yScale)
+    
+    // draw axes
+    svg
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${height - margin.bottom})`)
+      .call(xAxis)
+
+    svg
+      .append('g')
+      .attr('transform', `translate(${margin.left}, 0)`)
+      .call(yAxis)
+
     draw()
   }
 
   function draw() {
-    // filter Utah data
-    const utahData = state.data
-      .filter(d => d.current_state === 'Utah')
-    console.log('Utah data >>', utahData) // diagnostic
-
-    // group data by year; create objects with years as keys
-    // 
-    const groupedByYear = d3.rollup(utahData, i => {
-      const firstInstance = i[0]
-      return {
-        from_different_state_total: firstInstance.from_different_state_total,
-        abroad_total: firstInstance.abroad_total,
-        utah_total: firstInstance.population,
-      }
-    }, d => d.year)
-
-    console.log('grouped by year >>', groupedByYear) // diagnostic
-
-    // Convert Map objects to array of objects
-    const aggByYear = Array.from(groupedByYear, ([year, values]) =>
-      ({year: new Date(year), ...values}))
-
-    console.log('agg by year >>', aggByYear) // diagnostic
-
-    let totalGrowthFromDifferentStates = 0
-    let totalGrowthFromAbroad = 0
-    let totalGrowthState = 0 
-    let previousYearPop = 0
-
-    aggByYear.forEach(d => {
-      totalGrowthFromDifferentStates += d.from_different_state_total
-      totalGrowthFromAbroad += d.abroad_total
-
-      if (previousYearPop > 0) {
-        totalGrowthState += d.utah_total - previousYearPop
-      }
-      previousYearPop = d.utah_total
-    })
-
-    // prep data for barchart
-    const simpleBarChartData = [
-      { typeOfMigration : 'From Different States', value : totalGrowthFromDifferentStates },
-      { typeofMigration : 'From Abroad', value : totalGrowthFromAbroad },
-      { typeOfMigration : 'Population Growth', value : totalGrowthState },
-
-    ]
-    
-    console.log('bar chart data >>', simpleBarChartData)
 
   }
